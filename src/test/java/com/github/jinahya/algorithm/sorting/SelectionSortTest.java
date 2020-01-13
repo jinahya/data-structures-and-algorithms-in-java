8cc03e44bf89e0c1e9f035e0dcda875d4ff91b81
Jin Kwon <onacit@wemakeprice.com>
2020-01-13 14:06:00 +0900

    updated

--- src/test/java/com/github/jinahya/algorithm/sorting/SelectionSortTest.java
+++ src/test/java/com/github/jinahya/algorithm/sorting/SelectionSortTest.java
@@ -12,18 +12,26 @@ import static com.github.jinahya.algorithm.sorting.SelectionSort.sort;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
-import static java.util.Comparator.naturalOrder;
-import static java.util.Comparator.reverseOrder;
+import static java.util.Comparator.*;
 import static java.util.concurrent.ThreadLocalRandom.current;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.range;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
+/**
+ * A class for testing {@link SelectionSort}
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
 @Slf4j
 public class SelectionSortTest {
 
     // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Tests {@link SelectionSort#sort(int[], int, int)} method with an empty list.
+     */
     @Test
     public void testSortWithEmptyList() {
         final List<String> list = emptyList();
@@ -33,6 +41,9 @@ public class SelectionSortTest {
                 .isSortedAccordingTo(comparator);
     }
 
+    /**
+     * Test {@link SelectionSort#sort(int[], int, int)} method with a singleton list.
+     */
     @Test
     public void testSortWithSingletonList() {
         final List<Integer> list = singletonList(current().nextInt());
@@ -52,25 +63,29 @@ public class SelectionSortTest {
                 .map(i -> current().nextInt())
                 .boxed()
                 .collect(toList());
+        final List<Integer> expected = new ArrayList<>(unsorted);
+        {
+            Collections.sort(expected);
+        }
         final Comparator<Integer> comparator = naturalOrder();
         final List<Integer> actual = new ArrayList<>(unsorted);
         {
             sort(actual, comparator);
         }
-        final List<Integer> expected = new ArrayList<>(unsorted);
-        {
-            Collections.sort(expected);
-        }
         assertIterableEquals(expected, actual);
     }
 
+    /**
+     * Tests {@link SelectionSort#sort(List, Comparator)} method in reverse order.
+     */
     @Test
     public void testSortReverse() {
         final List<String> actual = new ArrayList<>();
         actual.add("3");
         actual.add("1");
+        actual.add(null);
         actual.add("2");
-        sort(actual, reverseOrder());
-        assertIterableEquals(asList("3", "2", "1"), actual);
+        sort(actual, nullsFirst(reverseOrder()));
+        assertIterableEquals(asList(null, "3", "2", "1"), actual);
     }
 }
