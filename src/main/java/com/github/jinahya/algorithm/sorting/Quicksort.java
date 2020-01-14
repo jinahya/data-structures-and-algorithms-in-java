baf04b59b07dae41b9f009eb69f87eb7da0c2061
Jin Kwon <onacit@wemakeprice.com>
2020-01-14 18:59:26 +0900

    updated

---
+++ src/main/java/com/github/jinahya/algorithm/sorting/Quicksort.java
@@ -0,0 +1,20 @@
+package com.github.jinahya.algorithm.sorting;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Quicksort {
+
+    // -----------------------------------------------------------------------------------------------------------------
+    public static <E> void sort(final List<? extends E> list, final Comparator<? super E> comparator) {
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Creates a new instance.
+     */
+    private Quicksort() {
+        super();
+    }
+}
