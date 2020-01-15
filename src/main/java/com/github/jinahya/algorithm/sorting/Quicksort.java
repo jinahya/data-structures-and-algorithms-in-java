144fbd5d8f7a083e2b784b3cc83103234b7e5156
Jin Kwon <onacit@wemakeprice.com>
2020-01-15 18:59:13 +0900

    updated

--- src/main/java/com/github/jinahya/algorithm/sorting/Quicksort.java
+++ src/main/java/com/github/jinahya/algorithm/sorting/Quicksort.java
@@ -1,50 +1,118 @@
 package com.github.jinahya.algorithm.sorting;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.BiFunction;
+import java.util.concurrent.atomic.LongAdder;
 
 import static java.util.Collections.swap;
 
+/**
+ * A class contains Quicksort implementations.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ * @see <a href="https://en.wikipedia.org/wiki/Quicksort">Quicksort (Wikipedia)</a>
+ */
+@Slf4j
 public class Quicksort {
 
-    // -----------------------------------------------------------------------------------------------------------------
-    public static <E> void sort(final List<E> list, final Comparator<? super E> comparator,
-                                final BiFunction<? super List<E>, ? super Comparator<? super E>, Integer> partitioner) {
+    // ---------------------------------------------------------------------------------------------------------- lomuto
+    static final ThreadLocal<LongAdder> LOMUTO_SWAP_COUNTER = new ThreadLocal<>();
+
+    private static <E> int partitionLomuto(final List<E> list, final Comparator<? super E> comparator) {
+        if (list == null) {
+            throw new NullPointerException("list is null");
+        }
+        if (list.isEmpty()) {
+            throw new IllegalArgumentException("list is empty");
+        }
+        if (comparator == null) {
+            throw new NullPointerException("comparator is null");
+        }
+        final int p = list.size() - 1;
+        int i = 0;
+        for (int j = 0; j < list.size() - 1; j++) {
+            if (comparator.compare(list.get(j), list.get(p)) < 0) { // not stable
+                swap(list, j, i++);
+                LOMUTO_SWAP_COUNTER.get().increment();
+            }
+        }
+        swap(list, p, i);
+        return i;
+    }
+
+    /**
+     * Sorts all elements in specified list using <a href="https://en.wikipedia.org/wiki/Quicksort#Lomuto_partition_scheme">Lomuto
+     * partition scheme</a>
+     *
+     * @param list       the list whose elements are sorted.
+     * @param comparator a comparator for comparing elements.
+     * @param <E>        element type parameter
+     */
+    public static <E> void sortLomuto(final List<E> list, final Comparator<? super E> comparator) {
         if (list == null) {
             throw new NullPointerException("list is null");
         }
         if (comparator == null) {
             throw new NullPointerException("comparator is null");
         }
-        if (partitioner == null) {
-            throw new NullPointerException("partitioner is null");
-        }
         if (list.size() < 2) {
             return;
         }
-        final int p = partitioner.apply(list, comparator);
+        final int p = partitionLomuto(list, comparator);
         assert p >= 0;
         assert p < list.size();
-        sort(list.subList(0, p), comparator, partitioner);
-        sort(list.subList(p + 1, list.size()), comparator, partitioner);
+        sortLomuto(list.subList(0, p), comparator);
+        sortLomuto(list.subList(p + 1, list.size()), comparator);
     }
 
-    public static <E> void lomuto(final List<E> list, final Comparator<? super E> comparator) {
-        sort(list,
-             comparator,
-             (l, c) -> {
-                 assert !l.isEmpty();
-                 final int p = l.size() - 1;
-                 int i = 0; // the index to be swapped with the pivot
-                 for (int j = 0; j < l.size() - 1; j++) {
-                     if (c.compare(l.get(j), l.get(p)) <= 0) {
-                         swap(l, j, i++);
-                     }
-                 }
-                 swap(l, p, i);
-                 return i;
-             });
+    // ----------------------------------------------------------------------------------------------------------- hoare
+    static final ThreadLocal<LongAdder> HOARE_SWAP_COUNTER = new ThreadLocal<>();
+
+    static <E> int partitionHoare(final List<E> list, final Comparator<? super E> comparator) {
+        assert !list.isEmpty();
+        final E p = list.get(list.size() / 2);
+        int i = -1;
+        int j = list.size();
+        while (true) {
+            do {
+                i++;
+            } while (comparator.compare(list.get(i), p) < 0);
+            do {
+                j++;
+            } while (comparator.compare(list.get(j), p) > 0);
+            if (i >= j) {
+                return j;
+            }
+            swap(list, i, j);
+            HOARE_SWAP_COUNTER.get().increment();
+        }
+    }
+
+    /**
+     * Sorts all elements in specified list using <a href="https://en.wikipedia.org/wiki/Quicksort#Hoare_partition_scheme">Hoare
+     * partition schema</a>.
+     *
+     * @param list       the list whose elements are sorted.
+     * @param comparator the comparator for comparing elements.
+     * @param <E>        element type parameter
+     */
+    public static <E> void sortHoare(final List<E> list, final Comparator<? super E> comparator) {
+        if (list == null) {
+            throw new NullPointerException("list is null");
+        }
+        if (comparator == null) {
+            throw new NullPointerException("comparator is null");
+        }
+        HOARE_SWAP_COUNTER.remove();
+        HOARE_SWAP_COUNTER.set(new LongAdder());
+        if (list.size() < 2) {
+            return;
+        }
+        final int p = partitionHoare(list, comparator);
+        sortHoare(list.subList(0, p + 1), comparator);
+        sortHoare(list.subList(p + 1, list.size()), comparator);
     }
 
     // -----------------------------------------------------------------------------------------------------------------
--- src/test/java/com/github/jinahya/algorithm/sorting/QuicksortTest.java
+++ src/test/java/com/github/jinahya/algorithm/sorting/QuicksortTest.java
@@ -1,5 +1,6 @@
 package com.github.jinahya.algorithm.sorting;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -7,11 +8,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.LongAdder;
 
-import static com.github.jinahya.algorithm.sorting.Quicksort.lomuto;
+import static com.github.jinahya.algorithm.sorting.Quicksort.LOMUTO_SWAP_COUNTER;
+import static com.github.jinahya.algorithm.sorting.Quicksort.sortLomuto;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
+import static java.util.Comparator.comparing;
 import static java.util.Comparator.naturalOrder;
 import static java.util.Comparator.nullsFirst;
 import static java.util.Comparator.reverseOrder;
@@ -25,11 +29,40 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 public class QuicksortTest {
 
     // -----------------------------------------------------------------------------------------------------------------
+    @Getter
+    static final class User {
+
+        public User(final int age, final String name) {
+            super();
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name + "(" + age + ")";
+        }
+
+        final int age;
+
+        final String name;
+    }
+
+    static List<User> users() {
+        final List<User> users = new ArrayList<>();
+        users.add(new User(3, "Three"));
+        users.add(new User(2, "John"));
+        users.add(new User(2, "Jane"));
+        users.add(new User(1, "One"));
+        return users;
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
     @Test
     public void testLomutoWithEmptyList() {
         final List<Integer> list = emptyList();
         final Comparator<Integer> comparator = naturalOrder();
-        lomuto(list, comparator);
+        sortLomuto(list, comparator);
         assertThat(list)
                 .isSortedAccordingTo(comparator);
     }
@@ -38,14 +71,11 @@ public class QuicksortTest {
     public void testLomutoWithSingletonList() {
         final List<Integer> list = singletonList(current().nextInt());
         final Comparator<Integer> comparator = naturalOrder();
-        lomuto(list, comparator);
+        sortLomuto(list, comparator);
         assertThat(list)
                 .isSortedAccordingTo(comparator);
     }
 
-    /**
-     * Tests {@link Quicksort#lomuto(List, Comparator)} method with a list of random elements.
-     */
     @Test
     public void testLomutoWithRandomElements() {
         final List<Integer> unsorted
@@ -62,7 +92,7 @@ public class QuicksortTest {
         final Comparator<Integer> comparator = naturalOrder();
         final List<Integer> actual = new ArrayList<>(unsorted);
         {
-            lomuto(actual, comparator);
+            sortLomuto(actual, comparator);
             assertThat(actual)
                     .isSortedAccordingTo(comparator);
         }
@@ -76,7 +106,21 @@ public class QuicksortTest {
         actual.add("1");
         actual.add(null);
         actual.add("2");
-        lomuto(actual, nullsFirst(reverseOrder()));
+        sortLomuto(actual, nullsFirst(reverseOrder()));
         assertIterableEquals(asList(null, "3", "2", "1"), actual);
+    }
+
+    @Test
+    public void testLomutoStability() {
+        final List<User> list = users();
+        log.debug("unsorted: {}", list);
+        final Comparator<User> comparator = comparing(User::getAge);
+        LOMUTO_SWAP_COUNTER.remove();
+        LOMUTO_SWAP_COUNTER.set(new LongAdder());
+        sortLomuto(list, comparator);
+        log.debug("sorted: {}", list);
+        assertThat(list)
+                .isSortedAccordingTo(comparator);
+        log.debug("swaps: {}", LOMUTO_SWAP_COUNTER.get().sum());
     }
 }
